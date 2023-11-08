a335ebc58fa7b7ec197978d1e4f5cd16b7d10142
erasthor <150274809+erasthor@users.noreply.github.com>
2023-11-08 12:48:14 -0400

    Add files via upload

---
+++ guardando eeprom/guardado2.c
@@ -0,0 +1,26 @@
+#include <18F4550.h>
+//#include <lcd.c>
+#device adc=10
+#FUSES NOWDT, HSPLL, PUT, NOPROTECT, NODEBUG, NOBROWNOUT, NOCPD, NOWRT, HS
+#use delay (clock=4M, crystal=4M)
+#use standard_io(A,B,D)
+
+
+
+void main(){
+
+int numero[100], n=0;
+//lcd_init();
+
+   while(True){
+   
+     for(int number = 0; number<256; number ++ ){
+     
+     write_eeprom(number, numero[2]);
+     
+    }//fin del for
+   
+   
+   }//fin del while
+   
+}//fin del void main
